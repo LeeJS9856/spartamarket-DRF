6f915b08ab9bcf51c2acee154929e934ee027faf
LeeJS9856 <wnstj1057@naver.com>
2024-04-29 15:17:42 +0900

    게시물 작성 구현

--- products/serializers.py
+++ products/serializers.py
@@ -4,5 +4,5 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'title', 'author', 'image', 'price', 'created_at')
-
+        fields = ('title', 'image', 'price')
+        read_only_fields = ("author",)
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
 from rest_framework.views import APIView, Response
+from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
 
@@ -9,3 +11,9 @@ class ProductAPIView(APIView) :
         serializers = ProductSerializer(products, many=True)
         print(serializers)
         return Response(serializers.data)
+    
+    def post(self, request) :
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True) :
+            serializer.save(author = request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
